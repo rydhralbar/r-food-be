32304b37059ddaee3bff4cdc1cab2b907aa3e836
Riyadh <albarrriyadh1112@gmail.com>
2023-03-12 09:33:29 +0800

    [UPDATE] Fix cors error

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['POST', 'GET', 'PATCH', 'DELETE']
+  })
+)
 
 app.use(helmet())
 
